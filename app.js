a4f1aa1cd367c09d1131f9f54078134a64403f25
Lance Liao <lancekupiak@gmail.com>
2018-11-16 21:27:03 +0800

    Add post reques logging

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/',(req,res) => {
-    res.send('Hello World');
+app.use(express.json());
+
+app.post('/', (req,res) => {
+    console.log(req.body);
+    res.send(req.body);
     res.end;
 });
+
 
 app.listen(process.env.PORT || 4000, ()=> {
     console.log('Server running...');
